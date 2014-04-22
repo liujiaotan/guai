0dd969d65873e99d1f50c6e47b47f88d09ed0358
liujiaotan <jellt.liu@gmail.com>
2014-04-22 11:33:39 +0800

    commit@2014-04-22AM by liujiaotan

---
+++ guai/guai/controllers/ConfigController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigController.h
+//  guai
+//
+//  Created by jellt.liu on 14-4-22.
+//  Copyright (c) 2014年 com.dcz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigController : UITableViewController
+
+@end
---
+++ guai/guai/controllers/StatusController.h
@@ -0,0 +1,13 @@
+//
+//  StatusController.h
+//  guai
+//
+//  Created by jellt.liu on 14-4-22.
+//  Copyright (c) 2014年 com.dcz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatusController : UITableViewController
+
+@end
